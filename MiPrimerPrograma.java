db9f5697d162e937791d2dcc6096a22c50bd335e
cristianq3 <cristianq3@gmail.com>
2023-06-14 21:44:19 -0300

    Se realizan Ejercicios de Hola mundo e imprimir variables

---
+++ MiPrimerPrograma.java
@@ -0,0 +1,26 @@
+
+public class MiPrimerPrograma {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Hola Mundo");                  
+      
+     boolean datoBooleano = true;
+     char UnaLetra = 'a';
+     String Texto = "Este es un tipo de dato String";
+     int Entero = 2;
+    long numeroLong = 2333333333333333L;
+   float decimales = 45.3f;
+
+     System.out.println("Hola Este es un booleano " + datoBooleano);  
+     System.out.println("Hola Este es una char " + UnaLetra);  
+     System.out.println("Hola Este es un String " + Texto);  
+     System.out.println("Hola Este es un int/Entero " + Entero);  
+     System.out.println("Hola Este es un long " + numeroLong);  
+     System.out.println("Hola Este es un float " + decimales);  
+
+     String nombre = "Cristian";
+     int edad = 36;
+     System.out.println("Hola Mi nombre es "+ nombre + " mi edad es "+edad+" años.");        
+    
+    }
+}
